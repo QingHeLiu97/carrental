0f45ee5fae89f156862f7746ffff681419254490
gfliu <email@qq.com>
2022-07-19 19:22:32 +0800

    2022-07-19 晚

--- src/main/java/com/yubin/controller/AdminController.java
+++ src/main/java/com/yubin/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.yubin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,6 +15,7 @@ public class AdminController {
     @ResponseBody
     @PostMapping("/login")
     public String login(HttpServletRequest request){
+        System.out.println("进入了登录页面");
         System.out.println(request);
         return null;
     }
